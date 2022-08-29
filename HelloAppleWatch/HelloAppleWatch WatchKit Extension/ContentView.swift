1deaee7e6a5591baa401d26a99655db97039be91
Christian Tisby <christian.d.tisby@gmail.com>
2022-08-29 02:47:22 -0400

    Completed Chapter 1 "Hello, Apple Watch"

--- HelloAppleWatch/HelloAppleWatch WatchKit Extension/ContentView.swift
+++ HelloAppleWatch/HelloAppleWatch WatchKit Extension/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @StateObject private var sentence = EmojiSentence()
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Image("cookie")
+                .resizable()
+                .scaledToFit()
+                .overlay(
+                
+                    Text(sentence.emoji)
+                        .font(.title3)
+                        .padding(.top, 10)
+                        .buttonStyle(.plain)
+                )
+            Text(sentence.text)
+                .font(.caption)
+                .padding(.top, 20)
+        }
+        .onTapGesture {
+            sentence.next()
+        }
     }
 }
 
