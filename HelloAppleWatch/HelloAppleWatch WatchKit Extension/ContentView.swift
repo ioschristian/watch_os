bf88a884efcc8adc2028bc5c44e1a2b75f4e1283
Christian Tisby <christian.d.tisby@gmail.com>
2022-08-29 01:38:53 -0400

    first commit

---
+++ HelloAppleWatch/HelloAppleWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by Christian Tisby on 8/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloAppleWatch/HelloAppleWatch WatchKit Extension/HelloAppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloAppleWatchApp.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by Christian Tisby on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
